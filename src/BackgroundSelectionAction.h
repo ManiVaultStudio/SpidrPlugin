a94d1d0e4523a6d1afe6e7a51242e26e5f51db84
alxvth <a.vieth@tudelft.nl>
2022-05-03 10:29:59 +0200

    Let user reset background dataset

--- src/BackgroundSelectionAction.h
+++ src/BackgroundSelectionAction.h
@@ -25,13 +25,18 @@ public: // Action getters
 
     /** Get smart pointer to dataset (if any) */
     hdps::Dataset<hdps::DatasetImpl> getBackgroundDataset() {
-        return _datasetPickerAction.getCurrentDataset();
+        if (_datasetPickerAction.isEnabled())
+            return _datasetPickerAction.getCurrentDataset();
+        else
+            return nullptr;
     }
 
     bool getIDsInData() { return _idsInDataAction.isChecked();  }
 
 protected:
     DatasetPickerAction     _datasetPickerAction;    /** Dataset picker action */
+    TriggerAction           _reloadDataSets;         /** Start computation action */
+    TriggerAction           _enableDisable;          /** Start computation action */
     ToggleAction            _idsInDataAction;        /** Tick if data contains IDs for background (useful for loading backgorund IDs), otherwise assume the data is a subset and use the global data IDs */
 
     friend class Widget;
